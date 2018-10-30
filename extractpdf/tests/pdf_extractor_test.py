eb807eac49a344a7df6429943ff9ec67589b5f73
Liad Magen <liad.magen@gmail.com>
2018-10-30 11:30:59 +0100

    fixed failing tests and added pylint to pipfile

--- extractpdf/tests/pdf_extractor_test.py
+++ extractpdf/tests/pdf_extractor_test.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from extractpdf.main import PDFExtractor
+from extractpdf.pdf_extractor import PDFExtractor
 
-class PDF2TextTest(unittest.TestCase):
-    def test_pdf2text_can_handle_empty_urls(self):
+class PDFExtracotTest(unittest.TestCase):
+    def test_extractpdf_can_handle_empty_urls(self):
         pe = PDFExtractor()
         self.assertRaises(AssertionError, pe.get_content, "")
 
-    def test_pdf2text_can_handle_broken_urls(self):
+    def test_extractpdf_can_handle_broken_urls(self):
         pe = PDFExtractor()
         self.assertRaises(AssertionError, pe.get_content, "https://arxiv.org/pdf/")
 
     @classmethod
-    def test_pdf2text_conversion(cls):
+    def test_extractpdf_conversion(cls):
         pe = PDFExtractor()
         c = pe.get_content("https://arxiv.org/pdf/1708.03615.pdf")
 
